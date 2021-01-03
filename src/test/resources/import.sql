d19c0d96463b2cf3c6e3bbff6b29b67d56dcf171
sunga201 <sung9255@gmail.com>
2021-01-03 23:08:27 +0900

    바꾼 put 메소드 테스트를 위해 initial data 변경.

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,4 +1,4 @@
-insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday, job) values (1, 'martin', 1996, 8, 15, 'engineer');
+insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (1, 'martin', 1996, 8, 15);
 insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (2, 'david', 1992, 9, 7);
 insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (3, 'dannis', 1990, 1, 13);
 insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (4, 'sophia', 1998, 8, 27);
