5c46921f708c91ac82a833c9be26ac64fb70d458
sunga201 <sung9255@gmail.com>
2021-01-03 20:47:46 +0900

    Block 엔티티 제거에 따른 테스트 케이스 업데이트.

--- src/main/java/com/practice/project/demo/Repository/BlockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice.project.demo.Repository;
-
-import com.practice.project.demo.entity.Block;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlockRepository extends JpaRepository<Block, Long> {
-
-}
--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,11 +1,5 @@
-insert into person(id, name, blood_type, year_of_birthday, month_of_birthday, day_of_birthday, job) values (1, 'martin', 'A', 1996, 8, 15, 'engineer');
-insert into person(id, name, blood_type, year_of_birthday, month_of_birthday, day_of_birthday) values (2, 'david', 'A', 1992, 9, 7);
-insert into person(id, name, blood_type, year_of_birthday, month_of_birthday, day_of_birthday) values (3, 'dannis', 'B', 1990, 1, 13);
-insert into person(id, name, blood_type, year_of_birthday, month_of_birthday, day_of_birthday) values (4, 'sophia', 'AB', 1998, 8, 27);
-insert into person(id, name, blood_type, year_of_birthday, month_of_birthday, day_of_birthday) values (5, 'benny', 'O', 2001, 3, 7);
-
-insert into block(id, name) values(1, 'martin');
-insert into block(id, name) values(2, 'sophia');
-
-update person set block_id = 1 where id = 1;
-update person set block_id = 2 where id = 4;
+insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday, job) values (1, 'martin', 1996, 8, 15, 'engineer');
+insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (2, 'david', 1992, 9, 7);
+insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (3, 'dannis', 1990, 1, 13);
+insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (4, 'sophia', 1998, 8, 27);
+insert into person(id, name, year_of_birthday, month_of_birthday, day_of_birthday) values (5, 'benny', 2001, 3, 7);
